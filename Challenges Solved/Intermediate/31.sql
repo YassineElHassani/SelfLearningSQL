d27c44f2094e59ceef29d8e0d2237e07ba6d84ad
Yassine El hassani <yassine.ds.404@gmail.com>
2025-02-11 13:43:21 +0100

    Challenge 31

---
+++ Challenges Solved/Intermediate/31.sql
@@ -0,0 +1,4 @@
+SELECT Customers.CustomerID, Orders.CustomerID
+FROM Customers 
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID AND Orders.EmployeeID = 4
+WHERE Orders.CustomerID IS NULL;
